d9202ab9b4d0d335320f2b4c21a71534992857f0
Alberto Mendoza <mendozaene@gmail.com>
2012-03-13 01:40:12 -0600

    Documentacion

--- application/modules/home/controllers/home.php
+++ application/modules/home/controllers/home.php
@@ -1,19 +1,21 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
- *
+ * Seccion principal de sistema
+ * @author 	N. Alberto Mendoza
+ * @author 	https://twitter.com/#!/_n3ri_
  */
 class Home extends CI_Controller {
 
 	/**
-	 * 
+	 * Constructor
 	 */
 	function __construct(){
 		parent::__construct();
 	}
 
 	/**
-	 * 
+	 * Muestra seccion principal
 	 */
 	public function index(){
 		$this->load->view('home_view');
