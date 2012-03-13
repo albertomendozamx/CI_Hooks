74bce0f2e037756237eb1bd88fbd4dbd4e1b2909
Alberto Mendoza <mendozaene@gmail.com>
2012-03-13 01:40:27 -0600

    Documentacion

--- application/modules/login/controllers/login.php
+++ application/modules/login/controllers/login.php
@@ -1,7 +1,11 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
- * 
+ * Controla el acceso a la aplicacion principal
+ * @author 	N. Alberto Mendoza
+ * @author 	https://twitter.com/#!/_n3ri_
+ * @todo 	encriptar clave
+ * @version 0.1
  */
 class Login extends CI_Controller {
 
@@ -13,14 +17,18 @@ class Login extends CI_Controller {
  	}
 
  	/**
- 	 * 
+ 	 * Muestra interfaz de acceso
  	 */
  	public function index(){
  		$this->load->view('login_view');
  	}
 
  	/**
+ 	 * Valida los datos de acceso ingresados por usuario y
+ 	 * redireccion segun resultado de evaluacion
  	 * 
+ 	 * @param 	string 	usuario de sistema
+ 	 * @param 	string 	clave de usuario
  	 */
  	public function validate($user=NULL, $pass=NULL){
  		$user = $this->input->post('user', TRUE);
@@ -29,10 +37,19 @@ class Login extends CI_Controller {
  		$stat = $this->Login_Model->match_data($user, $pass);
  		
  		if( $stat == TRUE ){
+ 			$this->session->set_userdata('logged_in','TRUE');
  			redirect('/home/');
  		} else {
  			redirect('');
  		}
+ 	}
+
+ 	/**
+ 	 * Termina la sesion de usuario
+ 	 */
+ 	public function logout(){
+ 		$this->session->sess_destroy();
+ 		redirect('');
  	}
 
 }
